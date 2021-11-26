f8dcef5049737ac41ed4b81dd1b2af0f443d83ea
Your Name <you@example.com>
2021-11-26 11:34:32 +0600

    Added search

--- forum/urls.py
+++ forum/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -14,4 +16,4 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('v1/api/categories/', CategoryListView.as_view()),
     path('v1/api/', include(router.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
-from rest_framework import generics, viewsets
+from django.db.models import Q
+from rest_framework import generics, viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Category, Article, ArticleImage
 from .serializers import CategorySerializer, ArticleSerializer, ArticleImageSerializer
@@ -21,4 +24,12 @@ class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
-
+    @action(detail=False, methods=['GET'])
+    def search(self, request, pk=None):
+        print(request.query_params)
+        q = request.query_params.get('q')
+        queryset = self.get_queryset()
+        queryset = queryset.filter(Q(title__icontains=q) |
+                                   Q(text__icontains=q))
+        serializer = ArticleSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
