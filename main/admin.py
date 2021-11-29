0197629485e6607a569259ba019973c744f47051
Your Name <you@example.com>
2021-11-29 13:01:45 +0600

    Comments added

--- main/admin.py
+++ main/admin.py
@@ -7,4 +7,13 @@ admin.site.register(Article)
 admin.site.register(Category)
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+
+
+admin.site.register(Comment, CommentAdmin)
+
+
 
